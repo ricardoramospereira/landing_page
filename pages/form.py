4c10c581dfcf8fee269b6421f11d24d45d191e5d
Ricardo <ricardoramospereira33@gmail.com>
2023-02-12 11:02:57 -0300

    test-form

---
+++ pages/form.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label="Nome")
+    email = forms.EmailField(label="Email")
+    subject = forms.CharField(label="Assunto")
+    menssage = forms.CharField(label="Mensagem", widget=forms.Textarea())
+    
--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,39 @@
 from django.shortcuts import render
 from .models import Teams, Servico, Plano
+from .form import ContactForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     teams = Teams.objects.all()
     servico = Servico.objects.all()
     plano = Plano.objects.all()
+    #form = ContactForm(request.Post or None)
+
+    '''if str(request.method) == 'POST':
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            menssage = form.cleaned_data['message']
+
+            print('Mensagem  enviada')
+            print(f'Nome: {name}')
+            print(f'Email: {email}')
+            print(f'Assunto: {subject}')
+            print(f'Mensagem: {menssage}')
+
+            messages.success(request, 'Enviado com sucesso')
+            form = ContactForm()
+        else:
+            messages.error(request, 'Erro ao enviar mensagem')'''
+
 
     data = {
         'teams': teams,
         'servico': servico,
         'plano': plano,
+        #'form': form,
     }
 
     return render(request, 'home.html', data)
